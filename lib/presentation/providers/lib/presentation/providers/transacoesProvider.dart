d337d90ee73fabd8a6fcecb875c286815c2ea439
agtransportes <103835196+agtransportes@users.noreply.github.com>
2026-05-25 00:03:21 +0200

    Create transacoesProvider.dart

---
+++ lib/presentation/providers/lib/presentation/providers/transacoesProvider.dart
@@ -0,0 +1,40 @@
+import '../../../listaDeImports.dart';
+
+class TransacoesProvider extends ChangeNotifier {
+  List<Transacoes> _transacoes = [];
+
+  List<Transacoes> get transacoes => _transacoes;
+
+  void adicionarTransacao(Transacoes transacao) {
+    _transacoes.add(transacao);
+    notifyListeners();
+  }
+
+  void removerTransacao(int index) {
+    _transacoes.removeAt(index);
+    notifyListeners();
+  }
+
+  void limparTransacoes() {
+    _transacoes = [];
+    notifyListeners();
+  }
+
+  List<Transacoes> transacoesPorPeriodo(DateTime inicio, DateTime fim) {
+    return _transacoes
+        .where((t) => t.data.isAfter(inicio) && t.data.isBefore(fim))
+        .toList();
+  }
+
+  List<Transacoes> transacoesDaSemana() {
+    final agora = DateTime.now();
+    final inicioSemana = agora.subtract(Duration(days: agora.weekday - 1));
+    return transacoesPorPeriodo(inicioSemana, agora);
+  }
+
+  List<Transacoes> transacoesDoMes() {
+    final agora = DateTime.now();
+    final inicioMes = DateTime(agora.year, agora.month, 1);
+    return transacoesPorPeriodo(inicioMes, agora);
+  }
+}
